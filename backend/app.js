11a19d65018497c18f43cf4b2c2c0cfdc3eff016
akshat200430 <akshatdeshmukhoff@gmail.com>
2026-03-04 00:08:07 +0530

    Added dotenv to backend dependencies

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@
 //     console.log(`App is listening on ${port}`)
 // })
 
-require('dotenv').config();
+require('dotenv').config();  
 
 const express = require('express');
 const mongoose = require('mongoose');
